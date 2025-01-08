491a8694ad12182ca8362967895cfc570e7b5396
Siraaj <morrissiraj@gmail.com>
2025-01-08 17:49:31 +0200

    p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,12 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import LearnMore from "@/components/LearnMore";
 import Navbar from "@/components/Navbar";
-<<<<<<< HEAD
 import ThreeProducts from "@/components/ThreeProducts";
 import ProductCarousel from "@/components/ProductCarousel";
-=======
-import ProductCarousel from "@/components/ProductCarousel";
-import ThreeProducts from "@/components/ThreeProducts";
 import ShopButton from "@/components/ShopButton";
 import ShopButton2 from "@/components/ShopButton2";
 import ShopButton3 from "@/components/ShopButton3";
 import ProductCard from "@/components/ProductCard";
-
->>>>>>> 392f4eca505f19b8b4c6c6e7cde115335dabe204
 
 export default function Home() {
   return (
